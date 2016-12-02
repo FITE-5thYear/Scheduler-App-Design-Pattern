64a90526874388dcfce1eb0d224875802e6d6f5a
HaAlaa <aalaa.alhamwi@hotmail.com>
2016-12-02 23:53:14 +0200

    Update Question4.java

--- src/Question4.java
+++ src/Question4.java
@@ -1,58 +1,48 @@
 
 public class Question4 {
-  
     public static void main(String[] args) {
        Client client = new Client();
   		
-        client.SetReminderStrategy(new VoiceAlert());
+       client.SetReminderStrategy(new VoiceAlert());
    	
-		client.SetReminderStrategy(new Notification());	
-    		
+       client.SetReminderStrategy(new Notification());		
     }
 }
 
 
- interface ISetting 
+interface ISetting 
 {  
-    String Reminder=null;
     String setting();
 }
- 
- //strategies
 
+ //strategies
  //Strategy 1:Voice Alter
- class VoiceAlert implements ISetting 
- {
+class VoiceAlert implements ISetting 
+{
     @Override
     public String setting() 
     {
        return "Voice alert";
     }
- }
+}
  
  //Strategy 2: Application Notification
- class Notification implements ISetting 
- {
+class Notification implements ISetting 
+{
     @Override
     public String setting() 
     {
        return "Notification";
-    }
-     
- }
+    }   
+}
  
 
- class Client 
- {
+class ChangeSetting 
+{
     private ISetting strategy;
- 
-    public String setting()
-    {
-        return strategy.setting();
-    }
  	
      //Change the strategy
-    public void SetReminderStrategy(ISetting strategy){
-        this.strategy = strategy;
+     public void SetReminderStrategy(ISetting strategy){
+         this.strategy = strategy;
     }
- }
+}
